09595a3b593c2051b509296148fbeb61b3e3c95d
Stephan Rubin <stephan.rubin@gmail.com>
2012-09-20 16:08:06 -0400

    fixed default route

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
@@ -56,6 +56,7 @@
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 Memome::Application.routes.draw do
+  root :to => 'welcome#index'
   resources :users
   resources :wisps
 end
